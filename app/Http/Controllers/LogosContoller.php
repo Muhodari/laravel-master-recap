2bf2d167807364ca5baa341888516c7369e07e20
Muhodari <sagemuho@gmail.com>
2022-04-09 11:15:52 +0200

    fixed issue

---
+++ app/Http/Controllers/LogosContoller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LogosContoller extends Controller
+{
+    //
+public function logos(){
+    return 'logos';
+}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PagesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('',[PagesController::class, 'index']);
 
 Route::get('login', function() {
     return view('login');
