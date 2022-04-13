fed2c71b332040f84327f1bb01a2eb0577ef3669
Muhodari <sagemuho@gmail.com>
2022-04-13 08:30:39 +0200

    code minimization  with layout

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Welcome to Laravel for Beginners from ProgrammingKnowledge</h1>
+<h1>Welcome to Laravel for Beginners </h1>
 <p> this is Laravel tutorial series. </p>
 
 @endsection
