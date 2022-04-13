f1a0bd720e68b89f5a3eeb19ccbd5d9b4336a93f
Muhodari <sagemuho@gmail.com>
2022-04-13 08:07:35 +0200

    accessing content from env file

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    
+
 public function index(){
-    return 'index';
+    return  view('pages.index');
 }
 
 
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{config('app.name','Laravel Project')}}</title>
+</head>
+<body>
+<h1>Welcome to Laravel for Beginners from ProgrammingKnowledge</h1>
+<p> this is Laravel tutorial series. </p>
+
+
+</body>
+</html>
