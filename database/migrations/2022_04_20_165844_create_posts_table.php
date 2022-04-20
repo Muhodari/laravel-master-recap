a17cd1daf1bfec7355d579799643871f04fd4a88
Muhodari <sagemuho@gmail.com>
2022-04-20 19:13:12 +0200

    done with models and migrations

--- database/migrations/2022_04_20_165844_create_posts_table.php
+++ database/migrations/2022_04_20_165844_create_posts_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->mediumText('body');
             $table->timestamps();
         });
     }
