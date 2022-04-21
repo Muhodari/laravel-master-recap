1c4e67a6482c754d960b67ab05b0e570d651aabd
Muhodari <sagemuho@gmail.com>
2022-04-21 21:50:35 +0200

    added migrations to assign posts to any user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,10 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+
+                    <a href="/posts/create" class="btn btn-primary">Create Post</a>
+                    <h3>Your Blog Posts</h3>
+
                 </div>
             </div>
         </div>
