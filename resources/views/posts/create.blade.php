17360535ddea031db0abd6967de32c55ae38b919
Muhodari <sagemuho@gmail.com>
2022-04-21 16:29:00 +0200

    proceeding

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,12 +7,15 @@
 
     {!! Form::open(['action' =>'\App\Http\Controllers\PostsController@store', 'method ' => 'POST ']) !!}
 
+
     <div class="form-group">
         {{Form::label('title','Title')}}
-
         {{Form::text('title','',['class' => 'form-control', 'placeholder' => 'Title'])}}
+    </div>
 
-
+    <div class="form-group">
+        {{Form::label('body','Body')}}
+        {{Form::text('body','',['class' => 'form-control', 'placeholder' => 'body'])}}
     </div>
 
     {!! Form::close() !!}
