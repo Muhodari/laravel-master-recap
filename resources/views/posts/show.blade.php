b14417a8e978fc73eda28d6d0ac1f0af3dbb6e7b
Muhodari <sagemuho@gmail.com>
2022-04-21 21:01:22 +0200

    added delete full functionality

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,19 @@
    <small>written on {{$post->created_at}}</small>
     <hr>
 
-    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+ <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+
+ {!! Form::open(
+    ['action'=>['\App\Http\Controllers\PostsController@destroy',$post->id],
+    ' method' => 'POST',
+    'class'  => 'pull-right'
+    ])!!}
+
+{{Form::hidden('_method','DELETE')}}
+ {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
+
+ {!! Form::close() !!}
+
 
 
 
