d82b60c9b533662518576a04eec3a37176520aad
Muhodari <sagemuho@gmail.com>
2022-04-22 15:37:45 +0200

    get the image here

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,19 +2,30 @@
 
 @section('content')
     <h1>Posts</h1>
-    @if(count($posts) > 1)
+    @if(count($posts) > 0)
 
         <div class="card">
             <ul class="list-group list-group-flush">
 
         @foreach($posts as $post)
 
+              <div class="row">
 
-                    <li class="list-group-item">
-                        <h3 ><a href="/posts/{{$post->id}}">{{ $post->title }}</a> </h3>
-                        <small>written on {{$post->created_at}} </small>
+                  <div class="col-md-4">
+                    <img style="width: 100%" src="/storage/cover_images/{{$post->cover_image}}" alt="">
+                  </div>
 
-                    </li>
+                  <div class="col-md-8">
+
+                          <h3 ><a href="/posts/{{$post->id}}">{{ $post->title }}</a></h3>
+                          <small>written on {{$post->created_at}} </small>
+
+                  </div>
+
+              </div>
+
+
+
 
          @endforeach
 
